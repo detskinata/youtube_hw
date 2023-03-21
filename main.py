2ff7b0c2482bbef18246b8e52da7b94d541a95cc
Natalia <detskina@list.ru>
2023-03-21 23:21:42 +0300

    2 task

--- main.py
+++ main.py
@@ -6,18 +6,53 @@ from dotenv import load_dotenv
 
 class Channel:
     def __init__(self, channel_id):
-        '''получение данных о ютуб канале по его id'''
-        self.channel_id = channel_id
+        '''получение данных о ютуб канале по его id.'''
+
+        self.__channel_id = channel_id
 
         load_dotenv()
         api_key: str = os.getenv('YOUTUBE_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/' + self.__channel_id
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self):
         '''вывод информации о канале'''
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
+    @property
+    def channel_id(self):
+        self.__channel_id
 
-#vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-#vdud.print_info()
+    def to_json(self, channel_json):
+        with open(channel_json, 'w', encoding='UTF=8') as file:
+            data = {
+                'id': self.__channel_id, 'title': self.title, 'description': self.description, 'url': self.url,
+                'subscriber_count': self.subscriber_count, 'video_count': self.video_count,
+                'view_count': self.view_count
+            }
+            return json.dump(data, file, indent=2, ensure_ascii=False)
+
+    @staticmethod
+    def get_service(self):
+        load_dotenv()
+
+        api_key: str = os.getenv('YOUTUBE_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+# vdud.print_info()
+# print(vdud.title)
+# print(vdud.video_count)
+# print(vdud.url)
+
+# vdud.channel_id = 'Новое название'
+vdud.to_json('vdud.json')
