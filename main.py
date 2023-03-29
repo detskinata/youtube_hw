f4feea4dfaf42439b56ee2fdebf6b91f0e01f0f2
Natalia <detskina@list.ru>
2023-03-29 22:07:00 +0300

    4 task

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Channel:
 
     @property
     def channel_id(self):
-        self.__channel_id
+        return self.__channel_id
 
     def to_json(self, channel_json):
         with open(channel_json, 'w', encoding='UTF=8') as file:
@@ -39,7 +39,7 @@ class Channel:
             return json.dump(data, file, indent=2, ensure_ascii=False)
 
     @staticmethod
-    def get_service(self):
+    def get_service() -> object:
         load_dotenv()
 
         api_key: str = os.getenv('YOUTUBE_KEY')
@@ -56,16 +56,52 @@ class Channel:
         return self.subscriber_count + other.subscriber_count
 
 
-ch1 = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-ch2 = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+class Video:
+    def __init__(self, video_id):
+        load_dotenv()
+        api_key: str = os.getenv('YOUTUBE_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.video_id = video_id
+        self.channel = Channel.get_service()
+        self.video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+        self.video_title = self.video['items'][0]['snippet']['title']
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
 
-vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-# vdud.print_info()
-# print(vdud.title)
-# print(vdud.video_count)
-# print(vdud.url)
+    def __str__(self) -> str:
+        return self.video_title
 
-# vdud.channel_id = 'Новое название'
-vdud.to_json('vdud.json')
-print(ch1)
-print(ch2)
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
+        load_dotenv()
+        api_key: str = os.getenv('YOUTUBE_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.playlists = youtube.playlists().list(id=self.playlist_id, part='snippet').execute()
+        self.playlist_title = self.playlists['items'][0]['snippet']['title']
+
+    def __str__(self) -> str:
+        return f"{self.video_title} {self.playlist_title}"
+
+# video1 = Video('9lO06Zxhu88')
+# print(video1)
+# video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+# print(video2)
+#
+# ch1 = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+# ch2 = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+#
+# vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+# # vdud.print_info()
+# # print(vdud.title)
+# # print(vdud.video_count)
+# # print(vdud.url)
+#
+# # vdud.channel_id = 'Новое название'
+# vdud.to_json('vdud.json')
+# print(ch1)
+# print(ch2)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import *
+from main import Channel, Video, PLVideo
 
 
 def test_str():
@@ -17,3 +17,23 @@ def test_add():
     ch1 = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     ch2 = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
     assert ch1.subscriber_count + ch2.subscriber_count == 14010000
+
+
+def test_video_init():
+    video1 = Video('9lO06Zxhu88')
+    assert video1.video_id == '9lO06Zxhu88'
+    assert video1.video_title == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
+
+
+def test_video_str():
+    video1 = Video('9lO06Zxhu88')
+    video1.__str__() == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
+
+
+def test_plvideo_init():
+    video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+    assert video2.playlist_title == 'Литература'
+
+def test_plvideo_str():
+    video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+    video2.__str__() == 'Пушкин: наше все? (Литература)'
