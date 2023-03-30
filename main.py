e4c101e6c5e98e342b064b7a197228c7c1a875e5
Natalia <detskina@list.ru>
2023-03-30 19:40:21 +0300

    4 task

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from googleapiclient.discovery import build
 import json
 from dotenv import load_dotenv
 
+load_dotenv()
+api_key: str = os.getenv('YOUTUBE_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 class Channel:
     def __init__(self, channel_id):
         '''получение данных о ютуб канале по его id.'''
 
         self.__channel_id = channel_id
-
-        load_dotenv()
-        api_key: str = os.getenv('YOUTUBE_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
         self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         self.title = self.channel['items'][0]['snippet']['title']
@@ -40,10 +40,6 @@ class Channel:
 
     @staticmethod
     def get_service() -> object:
-        load_dotenv()
-
-        api_key: str = os.getenv('YOUTUBE_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
     def __str__(self):
@@ -58,9 +54,6 @@ class Channel:
 
 class Video:
     def __init__(self, video_id):
-        load_dotenv()
-        api_key: str = os.getenv('YOUTUBE_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
         self.video_id = video_id
         self.channel = Channel.get_service()
         self.video = youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
@@ -77,31 +70,9 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-        load_dotenv()
-        api_key: str = os.getenv('YOUTUBE_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
         self.playlists = youtube.playlists().list(id=self.playlist_id, part='snippet').execute()
         self.playlist_title = self.playlists['items'][0]['snippet']['title']
 
     def __str__(self) -> str:
         return f"{self.video_title} {self.playlist_title}"
 
-# video1 = Video('9lO06Zxhu88')
-# print(video1)
-# video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-# print(video2)
-#
-# ch1 = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-# ch2 = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
-#
-# vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-# # vdud.print_info()
-# # print(vdud.title)
-# # print(vdud.video_count)
-# # print(vdud.url)
-#
-# # vdud.channel_id = 'Новое название'
-# vdud.to_json('vdud.json')
-# print(ch1)
-# print(ch2)
